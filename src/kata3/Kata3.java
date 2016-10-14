81f361dd6483c487b5b5138e5678d59e4612f601
Geraldo <Geraldo@10.230.169.57>
2016-10-14 12:04:19 +0100

    Version 1

---
+++ src/kata3/Kata3.java
@@ -0,0 +1,10 @@
+package kata3;
+
+public class Kata3 {
+
+    public static void main(String[] args) {
+        HistogramDisplay hito = new HistogramDisplay();
+        hito.execute();
+    }
+    
+}
